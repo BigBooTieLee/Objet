ffa9d02b88d4aefdf57e3a13687118a887f9ce8b
BigBooTieLee <147534783+BigBooTieLee@users.noreply.github.com>
2024-09-22 03:01:58 +0000

    TTONU

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Person lee = new Person();
+        lee.name = "Lee";
+        lee.n = 2007;
+         lee.aime = " et J'aime les jeux de vidéos";
+         lee.speak();  
+ }
+ 
+        
+
+        
+
+   }
+
+
---
+++ Person.java
@@ -0,0 +1,10 @@
+class Person {
+    // 
+    String name;
+    String aime;
+    int n;
+    // méthode
+    void speak() {
+        System.out.println("Bonjour je m'appelle " + name + ", je suis née en " + n + aime );
+    }
+}
